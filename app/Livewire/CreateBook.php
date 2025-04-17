09bc67fc40879fc4002b08238f4beac720d6eb13
Swayam <subinghi22@gmail.com>
2025-04-17 16:36:49 +0545

    Added validation rules and error display meassage

--- app/Livewire/CreateBook.php
+++ app/Livewire/CreateBook.php
@@ -4,14 +4,25 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Book;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Rule;
 
 class CreateBook extends Component
 {
+    #[Rule('string|min:3|max:255|required')]
     public $title;
+
+    #[Rule('string|required|max:20')]
     public $author;
+
+    #[Rule('integer|required|min:1|max:10')]
     public $rating;
 
+    #[Title('Add a book')]
+
     public function save(){
+
+        $this->validate();
         Book::create([
             'title' => $this->title,
             'author' => $this->author,
